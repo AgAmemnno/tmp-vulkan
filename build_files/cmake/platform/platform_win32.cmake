dbeb1091245a5da7d3a8bd7e1aa084597b1d95d5
AgAmemnno <kaz380@hotmail.co.jp>
2023-01-03 22:17:15 +0900

    Add manifest for draw_testing.

--- build_files/cmake/platform/platform_win32.cmake
+++ build_files/cmake/platform/platform_win32.cmake
@@ -113,7 +113,7 @@ add_definitions(-D_WIN32_WINNT=0x603)
 
 # First generate the manifest for tests since it will not need the dependency on the CRT.
 configure_file(${CMAKE_SOURCE_DIR}/release/windows/manifest/blender.exe.manifest.in ${CMAKE_CURRENT_BINARY_DIR}/tests.exe.manifest @ONLY)
-
+configure_file(${CMAKE_SOURCE_DIR}/release/windows/manifest/blender.exe.manifest.in ${CMAKE_CURRENT_BINARY_DIR}/draw_testing.exe.manifest @ONLY)
 if(WITH_WINDOWS_BUNDLE_CRT)
   set(CMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP TRUE)
   set(CMAKE_INSTALL_UCRT_LIBRARIES TRUE)

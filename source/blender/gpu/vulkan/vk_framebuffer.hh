dda4a66a81f379639ba8d965bb89def3ac248003
AgAmemnno <kaz380@hotmail.co.jp>
2023-02-01 22:46:00 +0900

    Added TextureSubImage to VKTexture.

--- source/blender/gpu/vulkan/vk_framebuffer.hh
+++ source/blender/gpu/vulkan/vk_framebuffer.hh
@@ -11,8 +11,6 @@
 #include "vk_context.hh"
 #include "vk_texture.hh"
 
-
-
 namespace blender::gpu {
 
 class VKStateManager;
@@ -39,19 +37,18 @@ class VKFrameBuffer : public FrameBuffer {
   /** True is the frame-buffer has been bound using the GL_FRAMEBUFFER_SRGB feature. */
   bool enabled_srgb_ = false;
 
-
-  int  flight_ticket_ = -1;
+  int flight_ticket_ = -1;
   bool is_render_begin_ = false;
   bool is_command_begin_ = false;
   Vector<VkSemaphore> submit_signal_;
   bool is_offscreen_signaled_ = false;
- public:
 
-   bool is_dirty_render_ = false;
+ public:
+  bool is_dirty_render_ = false;
   /**
    * Create a conventional frame-buffer to attach texture to.
    */
-  VKFrameBuffer(const char *name, VKContext* ctx);
+  VKFrameBuffer(const char *name, VKContext *ctx);
 
   /**
    * Special frame-buffer encapsulating internal window frame-buffer.
@@ -62,8 +59,8 @@ class VKFrameBuffer : public FrameBuffer {
    * \param w: Buffer width.
    * \param h: Buffer height.
    */
-  VKFrameBuffer(const char *name, VKContext *ctx,  int w, int h);
-  
+  VKFrameBuffer(const char *name, VKContext *ctx, int w, int h);
+
   ~VKFrameBuffer();
 
   void bind(bool enabled_srgb) override;
@@ -98,15 +95,15 @@ class VKFrameBuffer : public FrameBuffer {
    * Copy \a src at the give offset inside \a dst.
    */
   void blit(uint read_slot,
-                           uint src_x_offset,
-                           uint src_y_offset,
-                           VKFrameBuffer *metal_fb_write,
-                           uint write_slot,
-                           uint dst_x_offset,
-                           uint dst_y_offset,
-                           uint width,
-                           uint height,
-                           eGPUFrameBufferBits blit_buffers);
+            uint src_x_offset,
+            uint src_y_offset,
+            VKFrameBuffer *metal_fb_write,
+            uint write_slot,
+            uint dst_x_offset,
+            uint dst_y_offset,
+            uint width,
+            uint height,
+            eGPUFrameBufferBits blit_buffers);
 
   void blit_to(eGPUFrameBufferBits planes,
                int src_slot,
@@ -131,69 +128,81 @@ class VKFrameBuffer : public FrameBuffer {
     return is_srgb_;
   };
 
-  void set_srgb(bool t) {
+  void set_srgb(bool t)
+  {
     srgb_ = t;
   };
 
-   
-  VkCommandBuffer render_begin(VkCommandBuffer cmd, VkCommandBufferLevel level = VK_COMMAND_BUFFER_LEVEL_SECONDARY, VkClearValue* clearValues = nullptr,bool blit = false);
+  VkCommandBuffer render_begin(VkCommandBuffer cmd,
+                               VkCommandBufferLevel level = VK_COMMAND_BUFFER_LEVEL_SECONDARY,
+                               VkClearValue *clearValues = nullptr,
+                               bool blit = false);
   void render_end();
-  bool is_render_begin() {
+  bool is_render_begin()
+  {
     return is_render_begin_;
   }
 
-  VkSemaphore get_signal() {
-    return  submit_signal_[signal_index_];
+  VkSemaphore get_signal()
+  {
+    return submit_signal_[signal_index_];
   }
 
-  void  create_swapchain_frame_buffer(int);
-  VkCommandBuffer get_command() {
-    
-    if ( is_command_begin_ ) {
+  void create_swapchain_frame_buffer(int);
+  VkCommandBuffer get_command()
+  {
+
+    if (is_command_begin_) {
       cmd_refs++;
       return vk_cmd;
     }
 
     return VK_NULL_HANDLE;
-
   };
-  VkRenderPass get_render_pass() {
+  VkRenderPass get_render_pass()
+  {
     return vk_attachments_.renderpass_;
   }
-  VkImage        get_swapchain_image() {
+  VkImage get_swapchain_image()
+  {
     BLI_assert(is_swapchain_);
     return context_->get_current_image();
   }
-  VkImageLayout        get_swapchain_image_layout() {
+  VkImageLayout get_swapchain_image_layout()
+  {
     BLI_assert(is_swapchain_);
     return context_->get_current_image_layout();
   }
-  void set_swapchain_image_layout(VkImageLayout layout) {
+  void set_swapchain_image_layout(VkImageLayout layout)
+  {
     context_->set_current_image_layout(layout);
   };
 
-  void append_wait_semaphore(VkSemaphore sema) {
-    
-      wait_sema.push_back(sema);
+  void append_wait_semaphore(VkSemaphore sema)
+  {
 
+    wait_sema.push_back(sema);
   };
 
-  void set_dirty_render(bool t) {
+  void set_dirty_render(bool t)
+  {
     is_dirty_render_ = t;
   };
-private:
 
-  VkCommandBuffer     vk_cmd = VK_NULL_HANDLE;
-  int                             cmd_refs = 0;
-  VKAttachment      vk_attachments_;
+  void save_current_frame(const char *filename);
+
+ private:
+  VkCommandBuffer vk_cmd = VK_NULL_HANDLE;
+  int cmd_refs = 0;
+  VKAttachment vk_attachments_;
   bool is_nocolor_ = true;
   bool is_init_ = false;
   bool is_swapchain_ = false;
   bool is_blit_begin_ = false;
   int signal_index_ = -1;
-  int offscreen_render_times_  = 0;
+  int offscreen_render_times_ = 0;
 
-  std::vector<VkSemaphore>    wait_sema;
+  std::vector<VkSemaphore> wait_sema;
 
   bool use_multilayered_rendering_ = false;
 
@@ -202,12 +211,11 @@ private:
   /** Whether the primary Frame-buffer attachment is an SRGB target or not. */
   bool is_srgb_;
 
-  void init(VKContext* ctx);
+  void init(VKContext *ctx);
   void update_attachments();
   void force_clear();
 
-  VKContext* dirty_state_ctx_ = nullptr;
-
+  VKContext *dirty_state_ctx_ = nullptr;
 
   MEM_CXX_CLASS_ALLOC_FUNCS("VKFrameBuffer");
 };
@@ -218,11 +226,11 @@ private:
 
 template<typename T = GPUAttachmentType> VkImageLayout to_vk(const T type)
 {
-    switch (type) {
+  switch (type) {
     case GPU_FB_DEPTH_ATTACHMENT:
-        return VK_IMAGE_LAYOUT_DEPTH_ATTACHMENT_OPTIMAL;
+      return VK_IMAGE_LAYOUT_DEPTH_ATTACHMENT_OPTIMAL;
     case GPU_FB_DEPTH_STENCIL_ATTACHMENT:
-        return VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
+      return VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
     case GPU_FB_COLOR_ATTACHMENT0:
     case GPU_FB_COLOR_ATTACHMENT1:
     case GPU_FB_COLOR_ATTACHMENT2:
@@ -231,27 +239,24 @@ template<typename T = GPUAttachmentType> VkImageLayout to_vk(const T type)
     case GPU_FB_COLOR_ATTACHMENT5:
     case GPU_FB_COLOR_ATTACHMENT6:
     case GPU_FB_COLOR_ATTACHMENT7:
-        return VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+      return VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
     default:
-        BLI_assert(0);
-        return VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
-    }
+      BLI_assert(0);
+      return VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+  }
 
-    return VK_IMAGE_LAYOUT_UNDEFINED;
+  return VK_IMAGE_LAYOUT_UNDEFINED;
 }
 
 static inline VkImageAspectFlags to_vk(const eGPUFrameBufferBits bits)
 {
-    VkImageAspectFlags mask = 0x00;
-    mask |= (bits & GPU_DEPTH_BIT) ? VK_IMAGE_ASPECT_DEPTH_BIT : 0;
-    mask |= (bits & GPU_STENCIL_BIT) ? VK_IMAGE_ASPECT_STENCIL_BIT : 0;
-    mask |= (bits & GPU_COLOR_BIT) ? VK_IMAGE_ASPECT_COLOR_BIT : 0;
-    return mask;
+  VkImageAspectFlags mask = 0x00;
+  mask |= (bits & GPU_DEPTH_BIT) ? VK_IMAGE_ASPECT_DEPTH_BIT : 0;
+  mask |= (bits & GPU_STENCIL_BIT) ? VK_IMAGE_ASPECT_STENCIL_BIT : 0;
+  mask |= (bits & GPU_COLOR_BIT) ? VK_IMAGE_ASPECT_COLOR_BIT : 0;
+  return mask;
 }
-
 
 /** \} */
 
 }  // namespace blender::gpu
-
-
